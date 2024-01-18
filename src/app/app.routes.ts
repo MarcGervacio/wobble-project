af713d01be2d520c221da8f92f8579351ad1ad7e
Marc Gervacio <gervaciomarc@gmail.com>
2024-01-18 23:14:42 +0800

    feat(main) Added initial wobble project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+
+export const routes: Routes = [
+    { path: '', component: MainDashboardComponent }
+];
